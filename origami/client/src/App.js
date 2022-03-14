411ad86e479dc3682c9e36d7c4b03dcb6a66869e
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-03-14 15:51:31 +0200

    add main container

--- origami/client/src/App.js
+++ origami/client/src/App.js
@@ -1,12 +1,35 @@
+import { Component } from "react";
 import style from "./App.module.css";
 import Header from "./components/Header/Header/Header";
+import Menu from "./components/Header/Menu/Menu";
+import Main from "./components/Header/Main";
 
-function App() {
-  return (
-    <div className={style.app}>
-      <Header />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className={style.app}>
+//       <Header />
+
+//       <div className={style.container}>
+//         <Menu />
+//         <Main />
+//       </div>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
+        <Header />
+
+        <div className={style.container}>
+          <Menu />
+          <Main />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ origami/client/src/components/Header/Main/Main.js
@@ -0,0 +1,12 @@
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <main className="main-wrapper">
+      <h1>Sooooooooooooo Heading</h1>
+      <p>some posts</p>
+    </main>
+  );
+};
+
+export default Main;
