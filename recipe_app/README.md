8712c1cce5bbef5b1d54069bacfeb13760113e92
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-07-15 16:59:57 +0300

    add Popular component and styled it

---
+++ recipe_app/README.md
@@ -0,0 +1,17 @@
+# React Recipe app
+
+Web application for sharing photos and information about your secret recipes.
+
+The page is usable on mobile and desktop devices.
+
+## Used Technologies:
+
+-   [x] React Web
+
+-   [x] HTML
+
+-   [x] CSS / Styled Components
+
+-   [x] @Splidejs
+
+-   [x] spoonacular API
--- recipe_app/src/components/Popular.jsx
+++ recipe_app/src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -7,24 +10,92 @@ function Popular() {
     }, []);
 
     const getPopularRec = async () => {
-        const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-        );
-        const data = await api.json();
-        setPopular(data.recipes);
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+            );
+            const data = await api.json();
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+        }
     };
 
     return (
         <div>
-            {popular.map((rec) => {
-                return (
-                    <div key={rec.id}>
-                        <p>{rec.title}</p>
-                    </div>
-                );
-            })}
+            <Wrapper>
+                <h3>Popular Picks</h3>
+                <Splide
+                    options={{
+                        perPage: 3,
+                        arrows: false,
+                        pagination: false,
+                        drag: 'free',
+                        gap: '3rem',
+                    }}
+                >
+                    {popular.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img
+                                        src={recipe.image}
+                                        alt={recipe.title}
+                                    />
+                                    <Gradient />
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })}
+                </Splide>
+            </Wrapper>
         </div>
     );
 }
+
+const Wrapper = styled.div`
+    margin: 4rem 2rem;
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    min-width: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+        border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p {
+        position: absolute;
+        color: #ffffff;
+        text-align: center;
+        font-size: 26px;
+        font-weight: bold;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, -50%);
+    }
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75));
+`;
 
 export default Popular;
