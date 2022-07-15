f5429187653554a380e2f7ebc8b8b4e3ab0cb8b5
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-07-16 00:02:41 +0300

    add router-dom

--- recipe_app/src/components/Category.jsx
+++ recipe_app/src/components/Category.jsx
@@ -1,26 +1,27 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const Category = () => {
     return (
         <List>
-            <div>
+            <NavLink to='/cuisine/Italian'>
                 <FaPizzaSlice />
                 <h4>Italian</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to='/cuisine/American'>
                 <FaHamburger />
                 <h4>American</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to='/cuisine/Italian'>
                 <GiNoodles />
                 <h4>Thai</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to='/cuisine/Italian'>
                 <GiChopsticks />
                 <h4>Japanese</h4>
-            </div>
+            </NavLink>
         </List>
     );
 };
