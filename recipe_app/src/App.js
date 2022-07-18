5b1b0df619e7bbd7876868ee206d8f0ea99e9373
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-07-18 16:07:25 +0300

    add logo and nav

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -2,11 +2,18 @@ import Category from './components/Category';
 import Pages from './pages/Pages';
 import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
+import styled from 'styled-components';
+import { GiKnifeFork } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 function App() {
     return (
         <div className='App'>
             <BrowserRouter>
+                <Nav>
+                    <GiKnifeFork />
+                    <Logo to={'/'}>Delicious Recipes</Logo>
+                </Nav>
                 <Search />
                 <Category />
                 <Pages />
@@ -14,5 +21,23 @@ function App() {
         </div>
     );
 }
+
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 1.5rem;
+    font-weight: 500;
+    font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+    padding: 2rem 0rem 4rem 0rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    svg {
+        font-size: 2rem;
+        margin-right: 1rem;
+    }
+`;
 
 export default App;
